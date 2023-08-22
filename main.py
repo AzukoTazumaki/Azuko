2ce263701a36451435aebb95b733ec548b1f9044
Azuko <tazumaki@mail.ru>
2023-08-22 19:39:59 +0300

    FastApi site v1.0.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -12,4 +13,9 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse('home_content/home.html', {"request": request})
+    last_releases = ['Supposed To Be', 'with love from Azuko', 'Лавина']
+    return templates.TemplateResponse('home_content/home.html', {"request": request, "last_releases": last_releases})
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
