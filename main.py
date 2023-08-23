29da2d6558a5ea04eaf8ce046f6dae155b066ac4
Azuko <tazumaki@mail.ru>
2023-08-23 17:54:57 +0300

    FastApi site v1.0.4

--- main.py
+++ main.py
@@ -18,7 +18,15 @@ async def index(request: Request):
         ['../../static/images/last_releases/1.jpg', 'with love from Azuko', 'Azuko'],
         ['../../static/images/last_releases/2.jpg', 'Лавина', 'Azuko, 88Ringo']
     ]
-    return templates.TemplateResponse('home_content/home.html', {"request": request, "last_releases": last_releases})
+    products = [
+        ['../../static/images/products/beats.jpg', '3000'],
+        ['../../static/images/products/mixing_and_mastering.jpg', '2000', '1500']
+    ]
+    return templates.TemplateResponse('home_content/home.html',
+                                      {"request": request,
+                                       "last_releases": last_releases,
+                                       "products": products
+                                       })
 
 
 if __name__ == '__main__':
