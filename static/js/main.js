aaf9f49d8fd02a00f76e351a3db4e716e3cfaccc
Azuko <tazumaki@mail.ru>
2023-08-23 10:35:10 +0300

    FastApi site 1.0.3

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,23 @@ document.addEventListener("DOMContentLoaded", function () {
         document.querySelector("body").classList.add("loaded");
     }, 500)
 });
+
+/* MAIN 2 BACKGROUND */
+const main_2_div = document.querySelector('.main_2');
+let images_main_2 = document.querySelectorAll('col-3');
+images_main_2.forEach((image) => {
+    image.addEventListener('mouseenter', (e) => {
+        main_2_div.style.backgroundImage = "url(../../static/images/last_releases/0.jpg)";
+    });
+    image.addEventListener('mouseleave', (e) => {
+        main_2_div.style.backgroundImage = ''
+    });
+})
+    
+function image_mouseenter() {
+    
+    
+}
+function image_mouseleave() {
+    body.style.backgroundImage = "";
+}
